7b459d91b72a9e9278b1ed9cc4663eb324eba51c
yongjunfan0723 <fanyongjun1123@163.com>
2020-08-20 11:50:20 +0800

    update test case

--- test/subscribe.spec.js
+++ test/subscribe.spec.js
@@ -20,82 +20,96 @@ describe("test SubscribeTask", function() {
     sandbox.restore();
     SubscribeFactory.destroy();
   });
-  it("test register", (done) => {
-    const subscribeInst = SubscribeFactory.init();
-    const registerRes = subscribeInst.register(TEST_NAME, testTask, polling, timer);
-    expect(typeof registerRes.taskMap.get(TEST_NAME).task).to.equal("function");
-    expect(registerRes.taskMap.get(TEST_NAME).polling).to.true;
-    expect(registerRes.taskMap.get(TEST_NAME).timer).to.equal(timer);
-    done();
-  });
-
-  it("test start", () => {
-    const subscribeInst = SubscribeFactory.init();
-    subscribeInst
-      .register(TEST_NAME, testTask, false, timer)
-      .on(TEST_NAME, (error, res) => {
-        if (error) {
-          console.log("error", error);
-        }
-        if (res) {
-          expect(res.msg).to.equal("pending request had been canceled");
-        }
-      })
-      .start(TEST_NAME);
-  });
-
-  it("test start is error", (done) => {
-    const subscribeInst = SubscribeFactory.init();
-    subscribeInst.register(TEST_NAME, testTask, false, timer);
-    const taskJob = subscribeInst.getTask(TEST_NAME);
-    const stub = sandbox.stub(taskJob, "task");
-    stub.rejects(new Error("start in error"));
-    taskJob.task().catch((error) => {
-      expect(error.message).to.equal("start in error");
+  describe("test register", function() {
+    it("test register task", (done) => {
+      const subscribeInst = SubscribeFactory.init();
+      const registerRes = subscribeInst.register(TEST_NAME, testTask, polling, timer);
+      expect(typeof registerRes.taskMap.get(TEST_NAME).task).to.equal("function");
+      expect(registerRes.taskMap.get(TEST_NAME).polling).to.true;
+      expect(registerRes.taskMap.get(TEST_NAME).timer).to.equal(timer);
       done();
     });
   });
 
-  it("test stopPolling", () => {
-    const subscribeInst = SubscribeFactory.init();
-    subscribeInst.register(TEST_NAME, testTask, polling, timer).start(TEST_NAME);
-    const res = subscribeInst.stopPolling(TEST_NAME);
-    expect(res.taskIdMap).to.be.empty;
-    expect(res.taskIdMap.get(TEST_NAME)).to.undefined;
+  describe("test start", function() {
+    it("test start task", () => {
+      const subscribeInst = SubscribeFactory.init();
+      subscribeInst
+        .register(TEST_NAME, testTask, false, timer)
+        .on(TEST_NAME, (error, res) => {
+          if (error) {
+            console.log("error", error);
+          }
+          if (res) {
+            expect(res.msg).to.equal("pending request had been canceled");
+          }
+        })
+        .start(TEST_NAME);
+    });
+
+    it("test start task is error", (done) => {
+      const subscribeInst = SubscribeFactory.init();
+      subscribeInst.register(TEST_NAME, testTask, false, timer);
+      const taskJob = subscribeInst.getTask(TEST_NAME);
+      const stub = sandbox.stub(taskJob, "task");
+      stub.rejects(new Error("start in error"));
+      taskJob.task().catch((error) => {
+        expect(error.message).to.equal("start in error");
+        done();
+      });
+    });
   });
 
-  it("test stopAll", () => {
-    const subscribeInst = SubscribeFactory.init();
-    subscribeInst.register(TEST_NAME, testTask, polling, timer).start(TEST_NAME);
-    subscribeInst.register(TEST_INFONAME, testTask1, polling, timer).start(TEST_INFONAME);
-    const res = subscribeInst.stopAll();
-    expect(res.taskIdMap).to.be.empty;
+  describe("test stopPolling", function() {
+    it("test stop polling task", () => {
+      const subscribeInst = SubscribeFactory.init();
+      subscribeInst.register(TEST_NAME, testTask, polling, timer).start(TEST_NAME);
+      const res = subscribeInst.stopPolling(TEST_NAME);
+      expect(res.taskIdMap).to.be.empty;
+      expect(res.taskIdMap.get(TEST_NAME)).to.undefined;
+    });
   });
 
-  it("test removeTask", () => {
-    const subscribeInst = SubscribeFactory.init();
-    subscribeInst.register(TEST_NAME, testTask, polling, timer);
-    subscribeInst.register(TEST_INFONAME, testTask1, polling, timer);
-    const res = subscribeInst.removeTask(TEST_NAME);
-    expect(res.taskMap.get(TEST_NAME)).to.undefined;
+  describe("test stopAll", function() {
+    it("test stop all task", () => {
+      const subscribeInst = SubscribeFactory.init();
+      subscribeInst.register(TEST_NAME, testTask, polling, timer).start(TEST_NAME);
+      subscribeInst.register(TEST_INFONAME, testTask1, polling, timer).start(TEST_INFONAME);
+      const res = subscribeInst.stopAll();
+      expect(res.taskIdMap).to.be.empty;
+    });
   });
 
-  it("test removeAll", () => {
-    const subscribeInst = SubscribeFactory.init();
-    subscribeInst.register(TEST_NAME, testTask, polling, timer);
-    subscribeInst.register(TEST_INFONAME, testTask1, polling, timer);
-    const res = subscribeInst.removeAll();
-    expect(res.taskMap).to.be.empty;
-    expect(res.taskMap.get(TEST_NAME)).to.undefined;
-    expect(res.taskMap.get(TEST_INFONAME)).to.undefined;
+  describe("test removeTask", function() {
+    it("test remove task", () => {
+      const subscribeInst = SubscribeFactory.init();
+      subscribeInst.register(TEST_NAME, testTask, polling, timer);
+      subscribeInst.register(TEST_INFONAME, testTask1, polling, timer);
+      const res = subscribeInst.removeTask(TEST_NAME);
+      expect(res.taskMap.get(TEST_NAME)).to.undefined;
+    });
   });
 
-  it("test getTask", () => {
-    const subscribeInst = SubscribeFactory.init();
-    subscribeInst.register(TEST_NAME, testTask, polling, timer);
-    const res = subscribeInst.getTask(TEST_NAME);
-    expect(res).to.not.be.empty;
-    expect(res.polling).to.true;
-    expect(res.timer).to.equal(timer);
+  describe("test removeAll", function() {
+    it("test remove all task", () => {
+      const subscribeInst = SubscribeFactory.init();
+      subscribeInst.register(TEST_NAME, testTask, polling, timer);
+      subscribeInst.register(TEST_INFONAME, testTask1, polling, timer);
+      const res = subscribeInst.removeAll();
+      expect(res.taskMap).to.be.empty;
+      expect(res.taskMap.get(TEST_NAME)).to.undefined;
+      expect(res.taskMap.get(TEST_INFONAME)).to.undefined;
+    });
+  });
+
+  describe("test getTask", function() {
+    it("test get task", () => {
+      const subscribeInst = SubscribeFactory.init();
+      subscribeInst.register(TEST_NAME, testTask, polling, timer);
+      const res = subscribeInst.getTask(TEST_NAME);
+      expect(res).to.not.be.empty;
+      expect(res.polling).to.true;
+      expect(res.timer).to.equal(timer);
+    });
   });
 });
